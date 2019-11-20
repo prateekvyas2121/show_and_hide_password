f9dd6c649c53c15fa437752a68ae53eb500ec4ed
prateekvyas2121 <praatikmvyas4@gmail.com>
2019-11-20 19:40:52 +0530

    end of project

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
   </head>
   <body>
 <div class="container" style="margin-top: 10%;" >
-	<div class="row">
-		<div class="col-md-8 card">
+	<h2 align="center" >LOGIN Form</h2>
+	<div style="margin-left: 24%;" class="row">
+		<div  class="col-md-8 card">
 		<!-- BOOTSTRAP FORM START -->
-		    <form method="post" > 
+		    <form id="login_form" method="post" > 
 			  <div class="form-group row">
 			    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
 			    <div class="col-sm-10">
@@ -30,8 +31,8 @@
 			  </div>
 			  <div class="form-group row">
 			  	<div class="col-sm-10">
-				    <button class="btn btn-success" id="submit" type="submit">
-				    	submit
+				    <button class="btn btn-success" id="show_password" type="submit">
+				    	Show Password
 				    </button>
 				</div>
 			  </div>
@@ -49,7 +50,29 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).ready(function () {
+		$("#show_password").click(function (e) {
+			e.preventDefault();
+			// alert("hi");
+			var password = $("#inputPassword");
+			// alert(password)
+			var pwd_field_type = password.attr('type');
+			// alert(pwd_field_type);
+			if (pwd_field_type=='password') {
+				$("#inputPassword").prop('type','text');
+				// alert("working fine")
+				$(this).text("Hide")
+			}
+			else{
+				$("#inputPassword").prop('type','password');
+				// alert("working fine")
+				$(this).text("Show")
+			}
 
+		})
+	});
+</script>
 
   </body>
 </html>
